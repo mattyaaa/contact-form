5b9d73e1d7d1ec720010389bdea9a5f36362461f
matsumoto kaoru <bc.rw0130@gmail.com>
2024-12-18 20:06:06 +0900

    コミット

---
+++ src/app/Http/Controllers/ContactController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ContactController extends Controller
+{
+    //フォーム入力ページを呼び出す
+    public function index()
+    {
+        return view('index');
+    }
+
+    //送信ボタンクリック時に行われる処理
+    public function confirm(Request $request)
+    {
+        $contact = $request->only(['name', 'email', 'tel', 'content']);
+        return $contact;
+    }
+}
