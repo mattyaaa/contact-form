086b973177f67557c5e1b8d27c3103e11d02b413
matsumoto kaoru <bc.rw0130@gmail.com>
2024-12-21 22:31:25 +0900

    共通部分の layout 化

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -1,25 +1,11 @@
-<!DOCTYPE html>
-<html lang="ja">
+@extends('layouts.app')
 
-<head>
-  <meta charset="UTF-8" />
-  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>Contact Form</title>
-  <link rel="stylesheet" href="{{ asset('css/sanitize.css') }}" />
-  <link rel="stylesheet" href="{{ asset('css/index.css') }}" />
-</head>
+@section('css')
+<link rel="stylesheet" href="{{ asset('css/index.css') }}">
+@endsection
 
-<body>
-  <header class="header">
-    <div class="header__inner">
-      <a class="header__logo" href="/">
-        Contact Form
-      </a>
-    </div>
-  </header>
+@section('content')
 
-  <main>
     <div class="contact-form__content">
       <div class="contact-form__heading">
         <h2>お問い合わせ</h2>
@@ -80,7 +66,7 @@
           </div>
           <div class="form__group-content">
             <div class="form__input--textarea">
-              <textarea name="content" placeholder="資料をいただきたいです"></textarea>
+              <textarea name="content" placeholder="資料をいただきたいです">{{ old('content') }}</textarea>
             </div>
           </div>
         </div>
@@ -89,7 +75,4 @@
         </div>
       </form>
     </div>
-  </main>
-</body>
-
-</html>
+@endsection
