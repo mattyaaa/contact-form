7bd9a61462591bfbfadb104cbc909a89a7919bf3
matsumoto kaoru <bc.rw0130@gmail.com>
2024-12-21 17:45:52 +0900

    フォームリクエストの設定

--- src/app/Http/Controllers/ContactController.php
+++ src/app/Http/Controllers/ContactController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 //Contactモデルの追加
 use App\Models\Contact;
+//ContactRequestの追加
+use App\Http\Requests\ContactRequest;
 
 class ContactController extends Controller
 {
@@ -15,14 +17,14 @@ class ContactController extends Controller
     }
 
     //フォーム入力ページ送信ボタンクリック時に行われる処理
-    public function confirm(Request $request)
+    public function confirm(ContactRequest $request)
     {
         $contact = $request->only(['name', 'email', 'tel', 'content']);
         return view('confirm', compact('contact'));
     }
 
     //入力内容確認ページの送信ボタンクリック時に行われる処理
-    public function store(Request $request)
+    public function store(ContactRequest $request)
     {
         $contact = $request->only(['name', 'email', 'tel', 'content']);
         //保存処理
